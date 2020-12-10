26bfb80237d7187cc04a425e204e098e03ad3525
Madipta <madipta@gmail.com>
2020-12-10 23:15:21 +0700

    update hero

--- projects/ngtail/src/lib/components/heroes/hero/hero.component.ts
+++ projects/ngtail/src/lib/components/heroes/hero/hero.component.ts
@@ -3,14 +3,14 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'anu-hero',
   template: `
-    <section class="text-gray-700 body-font">
-      <div class="bg-gray-100 rounded container mx-auto flex pt-6 pb-24 mt-12 md:flex-row flex-col items-center">
+    <section class="text-lg text-gray-700 body-font">
+      <div class="bg-gradient-to-b from-transparent to-indigo-200 rounded-lg container mx-auto flex pt-12 pb-24 mt-6 md:flex-row flex-col items-center">
         <div class="lg:flex-grow md:w-1/2 px-8 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900" [innerHTML]="title"></h1>
+          <h1 class="title-font sm:text-4xl text-3xl font-bold text-gray-800 mb-4" [innerHTML]="title"></h1>
           <p class="mb-8 leading-relaxed" [innerHTML]="text"></p>
-          <div class="flex justify-center">
-            <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
-            <button class="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 rounded text-lg">Button</button>
+          <div class="flex justify-center text-lg">
+            <button class="inline-flex text-white tracking-wider bg-indigo-500 border-0 py-2 px-6 rounded">{{button1}}</button>
+            <button class="ml-3 inline-flex text-indigo-600 tracking-wider bg-white border-0 py-2 px-6 rounded">{{button2}}</button>
           </div>
         </div>
         <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
@@ -26,4 +26,6 @@ export class HeroComponent {
   @Input() title = 'Angular + TailwindCSS awesome!';
   @Input() text = 'Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.';
   @Input() imgUrl = 'https://dummyimage.com/720x600';
+  @Input() button1 = 'Register';
+  @Input() button2 = 'Sign In';
 }
