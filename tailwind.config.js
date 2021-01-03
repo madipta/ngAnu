dfa88fa26c7d02c8e7ac3222bc6d5abaf67e4d3a
Madipta <madipta@gmail.com>
2021-01-03 09:32:18 +0700

    fix tailwindcss postcss for production mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
-
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.ts'],
+  purge: {
+    enabled: true,
+    content: [
+      "./src/**/*.html",
+      "./src/**/*.ts",
+      "./projects/ngtail/src/**/*.html",
+      "./projects/ngtail/src/**/*.ts",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
   variants: {},
   plugins: [],
-}
+};
